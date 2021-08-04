a83520ad3ca0998c34e0098b47e6b14a011e1c52
PuneetSivananda <psivananda@chatsoft.com>
2021-08-04 21:09:38 +0530

    feat: added code for adding the tasks lists

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{ useState,useEffect } from 'react'
+import axios from 'axios'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        Hello World!! FARM
+      <div className="App list-group-item justify-content-center align-items-center mx-auto"
+        style={{
+          "width": "400px","backgroundColor": "white","marginTop": "15px"
+      }}
+      >
+        <h1 className="card tex-white bg-primary mb-1"
+          style={{"maxWidth":"20rem"}}>
+          Task Manager
+        </h1>
+      </div>
     </div>
   );
 }
