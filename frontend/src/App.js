405e3962be166a3c0ad042f21d1ca602fe6fafd3
PuneetSivananda <psivananda@chatsoft.com>
2021-08-05 17:34:50 +0530

    feat: added event handlers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,56 +5,77 @@ import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
+  const [todoList, setTodoList] = useState([{}]);
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+
+  useEffect(() => {
+    // Get all todos
+    axios.get("http://localhost:8000/api/todo").then((res) => {
+      setTodoList(res.data);
+    });
+  });
+
+  //Post a Todo
+  const addTodoHandler = () => {
+    axios
+      .post("http://localhost:8000/api/todo/", {
+        title: title,
+        description: desc,
+      })
+      .then((res) => console.log(res));
+  };
+
   return (
-    <div className='App'>
-      Hello World!! FARM
-      <div
-        className='App list-group-item justify-content-center align-items-center mx-auto'
-        style={{
-          width: "400px",
-          backgroundColor: "white",
-          marginTop: "15px",
-        }}
+    <div
+      className='App list-group-item justify-content-center align-items-center mx-auto'
+      style={{
+        width: "400px",
+        backgroundColor: "white",
+        marginTop: "15px",
+      }}
+    >
+      <h1
+        className='card tex-white bg-primary mb-1'
+        styleName='max-width: 20rem;'
       >
-        <h1
-          className='card tex-white bg-primary mb-1'
-          styleName='max-width: 20rem;'
-        >
-          Task Manager
-        </h1>
-        <h6 className='card text-white bg-primary mb-3'>
-          FASTAPI - React - Mongodb
-        </h6>
-        <div className='card-body'>
-          <h5 className='card text-white bg-dark mb-3'>Add Your Task</h5>
-          <span className='card-text'>
-            <input
-              type='text'
-              className='mb-2 form-control titleIn'
-              placeholder='Title'
-            />
-            <input
-              type='text'
-              className='mb-2 form-control descIn'
-              placeholder='Description'
-            />
-            <button
-              className='btn btn-outline-primary mx-2 mb-2'
-              style={{
-                borderRadius: "50px",
-                fontWeight: "bold",
-              }}
-            >
-              Add Task
-            </button>
-          </span>
-          <h5 className='card text-white bg-dark mb-3'>Your Tasks</h5>
-          <div className=''>{/* Todo Items - External Component */}</div>
-        </div>
-        <h6 className='card text-dark bg-warning py-1 mb-0'>
-          Copywright 2021, All Rights Reserved &copy;
-        </h6>
+        Task Manager
+      </h1>
+      <h6 className='card text-white bg-primary mb-3'>
+        FASTAPI - React - Mongodb
+      </h6>
+      <div className='card-body'>
+        <h5 className='card text-white bg-dark mb-3'>Add Your Task</h5>
+        <span className='card-text'>
+          <input
+            type='text'
+            className='mb-2 form-control titleIn'
+            placeholder='Title'
+            onChange={(event) => setTitle(event.target.value)}
+          />
+          <input
+            type='text'
+            className='mb-2 form-control descIn'
+            placeholder='Description'
+            onChange={(event) => setDesc(event.target.value)}
+          />
+          <button
+            className='btn btn-outline-primary mx-2 mb-2'
+            style={{
+              borderRadius: "50px",
+              fontWeight: "bold",
+            }}
+            onClick={addTodoHandler}
+          >
+            Add Task
+          </button>
+        </span>
+        <h5 className='card text-white bg-dark mb-3'>Your Tasks</h5>
+        <div className=''>{/* Todo Items - External Component */}</div>
       </div>
+      <h6 className='card text-dark bg-warning py-1 mb-0'>
+        Copywright 2021, All Rights Reserved &copy;
+      </h6>
     </div>
   );
 }
