843ab8ae72cb8e2bcb267dae81eae9be7681e5c5
PuneetSivananda <psivananda@chatsoft.com>
2021-08-05 15:38:53 +0530

    feat: ui changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,17 +6,35 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
-        Hello World!! FARM
-      <div className="App list-group-item justify-content-center align-items-center mx-auto"
+    <div className='App'>
+      Hello World!! FARM
+      <div
+        className='App list-group-item justify-content-center align-items-center mx-auto'
         style={{
-          "width": "400px","backgroundColor": "white","marginTop": "15px"
-      }}
+          width: "400px",
+          backgroundColor: "white",
+          marginTop: "15px",
+        }}
       >
-        <h1 className="card tex-white bg-primary mb-1"
-          style={{"maxWidth":"20rem"}}>
+        <h1
+          className='card tex-white bg-primary mb-1'
+          styleName='max-width: 20rem;'
+        >
           Task Manager
         </h1>
+        <h6 className='card text-white bg-primary mb-3'>
+          FASTAPI - React - Mongodb
+        </h6>
+        <div className='card-body'>
+          <h5 className='card text-white bg-dark mb-3'>Add Your Task</h5>
+          <span className='card-text'>
+            <input
+              type='text'
+              className='mb-2 form-control titleIn'
+              placeholder='Title'
+            />
+          </span>
+        </div>
       </div>
     </div>
   );
